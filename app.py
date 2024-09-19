42cfee5ad88de04f08cd38141eac55ff111ca84c
Mathisgdi <mathis.ghidotti@epitech.digital>
2024-09-19 21:10:40 +0200

    feat : del comment

--- app.py
+++ app.py
@@ -1,65 +1,3 @@
-# import joblib
-# import pandas as pd
-# import numpy as np
-# from fastapi import FastAPI, HTTPException, Request
-# from fastapi.middleware.cors import CORSMiddleware
-#
-#
-# app = FastAPI()
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Vous pouvez spécifier des domaines spécifiques ici
-#     allow_credentials=True,
-#     allow_methods=["*"],  # Permet toutes les méthodes (POST, GET, OPTIONS, etc.)
-#     allow_headers=["*"],  # Permet tous les headers
-# )
-# @app.post("/driver")
-# async def create_proba(request: Request):
-#     model = joblib.load('VVA6-class100/model.joblib')
-#
-#     try:
-#         data = await request.json()
-#         df = pd.read_json(data['data'])
-#     except Exception as e:
-#         raise HTTPException(status_code=400, detail=str(e))
-#
-#     # Récupérer les données envoyées par l'utilisateur via l'API
-#     # data = pd.read_csv("data/test.csv")
-#
-#     # predictions  = model.predict(data)
-#     # print(predictions)
-#
-#     predictions_proba = model.predict_proba(df)
-#
-#     # print(predictions_proba)
-#
-#     def assign_unique_positions(probabilities):
-#         num_pilots = probabilities.shape[0]
-#         num_positions = probabilities.shape[1]
-#
-#         assigned_positions = [-1] * num_pilots  # Initialiser avec -1 (non assigné)
-#         taken_positions = set()  # Ensemble des positions déjà prises
-#
-#         for i in range(num_pilots):
-#             sorted_indices = np.argsort(probabilities[i])[::-1]  # Trier les indices par probabilité décroissante
-#             for index in sorted_indices:
-#                 # On s'assure que la position est >= 1 (index 0 correspond au premier place)
-#                 if (index + 1) not in taken_positions:  # +1 pour que la position soit à partir de 1
-#                     assigned_positions[i] = index + 1  # On attribue une position qui commence à 1
-#                     taken_positions.add(index + 1)
-#                     break
-#
-#         # Si une position reste non assignée, attribuer la première disponible.
-#         available_positions = set(range(1, num_pilots + 1)) - taken_positions
-#         for i in range(num_pilots):
-#             if assigned_positions[i] == -1:
-#                 assigned_positions[i] = available_positions.pop()
-#         return assigned_positions
-#
-#     unique_positions = assign_unique_positions(predictions_proba)
-#     return {"prédiction": unique_positions}
-
-
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import pandas as pd
@@ -128,31 +66,8 @@ async def predict(data: DriverData):
     positions_data['Rainfall'] = data.rainfall
     positions_data = positions_data.rename(columns={'constructor': 'name'})
     positions_data = positions_data.rename(columns={'position': 'grid'})
-    # Assurez-vous que les colonnes sont dans le bon ordre et les types de données sont corrects
-    # positions_data = positions_data.astype({
-    #     'Rainfall': 'float64',
-    #     'Humidity': 'float64',
-    #     'AirTemp': 'float64',
-    #     'grid': 'int64'
-    # })
     print(positions_data)
-    #positions_data['grid'] = data.airTemp
-    # positions_data['AirTemp'] = data.airTemp
-    # positions_data['AirTemp'] = data.airTemp
 
-    # Faire la prédiction avec le modèle
-
-    # predictions_proba = model.predict_proba(positions_data)
-    # print(predictions_proba)
-    #
-    # # Attribuer des positions uniques
-    # unique_positions = assign_unique_positions(predictions_proba)
-    # # predictions_proba = model.predict_proba(positions_data)
-    # #
-    # # # Convertir les probabilités en liste
-    # # predictions_proba = predictions_proba.tolist()
-    # # Attribuer des positions uniques
-    # unique_positions = assign_unique_positions(np.array(predictions_proba))
 
     predictions_proba = model.predict_proba(positions_data)
     # Convertir les probabilités en liste
